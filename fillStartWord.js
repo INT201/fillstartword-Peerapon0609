cb4ac921b815f8467f8542d590791180bb4e1f62
Peerapon <peerapomsri2545@gmail.com>
2022-08-31 19:53:22 +0700

    third-commit

--- fillStartWord.js
+++ fillStartWord.js
@@ -4,7 +4,7 @@ function fillStartWord(startWord, word) {
   if(startWord!==word){
     return startWord+word
   }
-  else if(startWord===word){
+  if(startWord===word){
     return word
   }
   else if(word===null){
