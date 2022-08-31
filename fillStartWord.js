4ec5d203ef1495f138e0f18958b18f82f6d1286c
Peerapon <peerapomsri2545@gmail.com>
2022-08-31 19:36:25 +0700

    add-if-statement

--- fillStartWord.js
+++ fillStartWord.js
@@ -1,6 +1,14 @@
 const { template } = require('@babel/core')
 
 function fillStartWord(startWord, word) {
-  //code here
+  if(startWord!==word){
+    return startWord+word
+  }
+  else if(startWord===word){
+    return word
+  }
+  else if(startWord===word){
+    return word
+  }
 }
 module.exports = fillStartWord
